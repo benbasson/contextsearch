e25a98223a25584abd632637ed568d3000bdb1a2
BenBasson <github@basson.at>
2013-10-06 19:23:55 +0100

    Remove duplicate lines from licence header.

--- src/chrome/content/contextsearch.js
+++ src/chrome/content/contextsearch.js
@@ -10,9 +10,6 @@
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * License.
- * License.
- * License.
  *
  * The Original Code is Context Search.
  *
