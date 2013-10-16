a118655e4f76f9701306ec75b1018f71ed8aad07
BenBasson <github@basson.at>
2013-10-16 20:42:08 +0100

    Disable "click menu to search" by default.

--- src/defaults/preferences/contextsearch.js
+++ src/defaults/preferences/contextsearch.js
@@ -1,4 +1,4 @@
-pref("extensions.contextsearch.clickMenuToSearch",true);
+pref("extensions.contextsearch.clickMenuToSearch",false);
 pref("extensions.contextsearch.hideStandardContextItem",true);
 pref("extensions.contextsearch.quoteStringsWithSpaces",false);
 pref("extensions.contextsearch.separatorItems","");
